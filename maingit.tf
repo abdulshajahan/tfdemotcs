e381f0a1e5eba7d5acb5ccce3b3929a0e56d898b
shaj <shaj@tcs.com>
2025-05-21 16:54:50 +0530

    TF3rd commit

--- maingit.tf
+++ maingit.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "gitvpc" {
   cidr_block = "10.200.0.0/16"
   tags = {
-    Name = "gitbase-vpc200"
+    Name = "gitbase-vpc200-v2"
     developer = "gitbase"
     environment = "gitbase"
   }
